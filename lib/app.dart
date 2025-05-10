cda588c4ac89a00c99fd8519681a5bf57fe8416f
Ali-Tarek <alitarek115.at@gmail.com>
2025-05-10 20:27:16 +0300

    Implement product search, map view, directions, and UI improvements (point 6 & 7)

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,12 @@ import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 import 'screens/landing_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/product_search_screen.dart';
+import '../cubits/product/product_cubit.dart';
+import '../cubits/store/store_cubit.dart';
+import '../services/product_service.dart';
+import '../services/store_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -21,6 +27,17 @@ class MyApp extends StatelessWidget {
         '/register': (context) => RegisterScreen(),
         '/home': (context) => HomeScreen(),
         '/stores': (context) => StoresScreen(),
+        '/search-products': (context) => MultiBlocProvider(
+          providers: [
+            BlocProvider<ProductCubit>(
+              create: (_) => ProductCubit(ProductService()),
+            ),
+            BlocProvider<StoreCubit>(
+              create: (_) => StoreCubit(StoreService()),
+            ),
+          ],
+          child: ProductSearchScreen(),
+        ),
       },
     );
   }
